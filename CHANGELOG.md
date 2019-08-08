8afc885e6a66f10060a19cdf424a6a57ccb131cd
Tyr Chen <tyr.chen@gmail.com>
2019-08-08 16:32:54 -0700

    fix stub generator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1-alpha (August 08, 2019)
+- fix the stub generator
+
+
 ## 0.2.0 (August 07, 2019)
 - use grpc 0.4.0 tag
 
--- lib/client/stub.ex
+++ lib/client/stub.ex
@@ -8,9 +8,16 @@ defmodule GrpcBuilder.Client.StubGen do
 
   defp gen(args) do
     quote bind_quoted: [args: args] do
-      Enum.each(args[:services], fn {name, mod} ->
-        def unquote(name)(chan, req, opts \\ []),
-          do: apply(unquote(mod), unquote(name), [chan, req, opts])
+      Enum.each(args[:services], fn {name, data} ->
+        case data do
+          {mod, orig_name} ->
+            def unquote(name)(chan, req, opts \\ []),
+              do: apply(unquote(mod), unquote(orig_name), [chan, req, opts])
+
+          mod ->
+            def unquote(name)(chan, req, opts \\ []),
+              do: apply(unquote(mod), unquote(name), [chan, req, opts])
+        end
       end)
     end
   end
